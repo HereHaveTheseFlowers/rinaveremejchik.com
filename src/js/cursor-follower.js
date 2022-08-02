e4c97caaeea9614e424bead05d349b0db188678c
herehavetheseflowers <herehavetheseflowers@gmail.com>
2022-08-02 10:29:32 +0400

    Deploy Ready

---
+++ src/js/cursor-follower.js
@@ -0,0 +1,78 @@
+/// CURSOR REPLACEMENT
+
+let follower = $("#cursor-follower");
+let cursorshape = $("#cursor-shape");
+let cursortext = $("#cursor-text");
+let cursorzoom = $("#cursor-zoom");
+
+gsap.set(follower, {xPercent: -50, yPercent: -50});
+
+let xTo = gsap.quickTo(follower, "x", {duration: 0.1, ease: "power3"}),
+    yTo = gsap.quickTo(follower, "y", {duration: 0.1, ease: "power3"});
+
+window.addEventListener("mousemove", e => {
+	follower.css('opacity', '1');
+	xTo(e.clientX);
+	yTo(e.clientY);
+});
+
+$("svg.slider-prev").on("mouseenter", function() {
+	cursorshape.addClass(" show_hint_button");
+});
+
+$("svg.slider-prev").on("mouseleave", function() {
+	cursorshape.removeClass(" show_hint_button");
+});
+
+$("svg.slider-next").on("mouseenter", function() {
+	cursorshape.addClass(" show_hint_button");
+});
+
+$("svg.slider-next").on("mouseleave", function() {
+	cursorshape.removeClass(" show_hint_button");
+});
+
+$("#buttontext").on("mouseenter", function() {
+	cursorshape.addClass(" show_hint_button");
+});
+
+$("#buttontext").on("mouseleave", function() {
+	cursorshape.removeClass(" show_hint_button");
+});
+
+$("a.link-instagram").on("mouseenter", function() {
+	cursorshape.addClass(" show_hint_button");
+});
+
+$("a.link-instagram").on("mouseleave", function() {
+	cursorshape.removeClass(" show_hint_button");
+});
+
+$(".cursor-hover").on("mouseenter", function() {
+	if(dont_show_hint) return;
+	cursorshape.addClass(" show_hint_copy");
+	cursortext.addClass(" show_hint_copy");
+});
+
+$(".cursor-hover").on("mouseleave", function() {
+	cursorshape.removeClass(" show_hint_copy");
+	cursortext.removeClass(" show_hint_copy");
+});
+
+$(".zoomable").on("mouseenter", function() {
+	cursorshape.addClass(" show_hint_zoom");
+	cursorzoom.addClass(" show_hint_zoom");
+});
+
+$(".zoomable").on("mouseleave", function() {
+	cursorshape.removeClass(" show_hint_zoom");
+	cursorzoom.removeClass(" show_hint_zoom");
+});
+
+$("a").on("mouseenter", function() {
+	cursorshape.addClass(" show_hint_button");
+});
+
+$("a").on("mouseleave", function() {
+	cursorshape.removeClass(" show_hint_button");
+});
---
+++ src/js/modernizr-check.js
@@ -0,0 +1,14 @@
+// check for svg support
+if (!Modernizr.inlinesvg) {
+	$("#zoom-svg").css('display', 'none');
+	$("#zoom-svg-fallback").css('display', 'block');
+	$("#zoom-svg-fallback").attr("src", './img/zoom.png');
+
+	$("#icon_logo-svg").css('display', 'none');
+	$("#icon_logo-fallback").css('display', 'block');
+	$("#icon_logo-fallback").attr("src", './img/icon_logo.png');
+}
+if(!Modernizr.svg) {
+	$("img.link-instagram").attr("src", "./img/instagram.png");
+	$("img.zoomed-cross").attr("src", "./img/CROSS.png");
+}
